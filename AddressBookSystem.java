bc164052b6c1a043f7614e28494efdc951f629df
unknown <you@example.com>
2022-10-01 13:00:24 +0530

    Added : created a contact in addressbook

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.AddressBook;
 
+import java.util.ArrayList;
+
 public class AddressBookSystem {
     public static void main(String[] args) {
 
@@ -8,5 +10,26 @@ public class AddressBookSystem {
         * */
 
         System.out.println("Welcome to Address Book Program");
+        Contacts.createContact("Ullas Kumar K","Vijayanagar","Bangalore","Karnataka","560023","8105897193","ullaskumark29@gmail.com");
+    }
+
+
+    class Contacts {
+        /**creating contact using Arraylist **/
+
+        static void createContact(String name, String address, String city, String state, String zipCode, String phoneNo, String email) {
+            ArrayList<String> firstContact = new ArrayList<String>(7);
+
+            firstContact.add(name);
+            firstContact.add(address);
+            firstContact.add(city);
+            firstContact.add(state);
+            firstContact.add(zipCode);
+            firstContact.add(phoneNo);
+            firstContact.add(email);
+
+            System.out.println(firstContact );
+
+        }
     }
 }

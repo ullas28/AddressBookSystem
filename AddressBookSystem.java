bb178821f40ff5a74ff254b7ccb786f31f096749
unknown <you@example.com>
2022-10-01 12:19:03 +0530

    Added : Welcome to address book message

---
+++ AddressBookSystem.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.AddressBook;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+
+        /*
+        * Starting with displaying welcome to address program
+        * */
+
+        System.out.println("Welcome to Address Book Program");
+    }
+}
